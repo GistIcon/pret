29264a4ca448ef32090b644143aaa1415641a089
usernamealreadyis <usernamealreadyis@users.noreply.github.com>
2019-02-11 02:02:12 -0800

    PY
    •Reading Keyboard Input:
    Python provides two built-in functions to read a line of text from standard input, which by default comes from the keyboard

---
+++ #!/usr/bin/python/raw_input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+str = raw_input("Enter your input: ");
+print "Received input is : ", str
+
+#!/usr/bin/python
+
+str = input("Enter your input: ");
+print "Received input is : ", str
